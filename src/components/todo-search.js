0732e5eab9e69f1e0887ef870e1cd95f7709852a
sr-jerly <jerlymigueldelarosah@gmail.com>
2021-10-25 17:13:03 -0400

    add: Task Styles

--- src/components/todo-search.js
+++ src/components/todo-search.js
@@ -1,9 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 
 const TodoSearch =()=>{
     return (
-        <input placeholder="vikings"/>
+        <Input  placeholder="Tasks"/>    
     )
 }
 
 export default TodoSearch;
+
+
+
+const Input = styled.input`
+  display: flex;
+  margin: 4rem auto;
+  background: 0;
+  border: 0;
+  outline: none;
+  width: 80vw;
+  max-width: 400px;
+  font-size: 1.5em;
+  transition: 0.5s ease;
+  border-bottom: 1px solid  #00bcd4;
+  border-bottom-width: thin;
+  width: 20rem;
+
+  &:focus { 
+    width: 50rem;
+  }
+  
+`;
